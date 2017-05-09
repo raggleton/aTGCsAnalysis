aab16a6d990a83dcd4491db1c946864ec93586ea
muhammadansariqbal <muhammadansariqbal@gmail.com>
2017-05-09 17:28:11 +0200

    Pile up reweighting; Corrected a +1 in dumpPileup.py, modified PU_data_f array in PU.h for Summer16 MC.

--- Common/dumpPileup.py
+++ Common/dumpPileup.py
@@ -1,12 +1,13 @@
 import ROOT
 import os
-from SimGeneral.MixingModule.mix_2015_25ns_FallMC_matchData_PoissonOOTPU_cfi import mix as mix_2015_25ns_realistScenario
+#from SimGeneral.MixingModule.mix_2015_25ns_FallMC_matchData_PoissonOOTPU_cfi import mix as mix_2015_25ns_realistScenario
 
 
-filePileupData = ROOT.TFile("MyDataPileupHistogram.root")
+filePileupData = ROOT.TFile("../PUTrueDistProducer/data/MyDataPileupHistogram.root")
 histPUData = filePileupData.Get("pileup")
 histPUData.Scale(1./histPUData.Integral())
-for iBin in range(1, histPUData.GetNbinsX()):
+
+for iBin in range(1, histPUData.GetNbinsX()+1):
 	print histPUData.GetBinContent(iBin), ","
 
 #print sum(mix_2015_25ns_realistScenario.input.nbPileupEvents.probValue)
