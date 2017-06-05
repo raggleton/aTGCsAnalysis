9ac13b9fe99ddeeaf48ee8ce17aca3f4b4befcfd
Robin <raggleton@users.noreply.github.com>
2017-06-05 13:28:21 +0200

    Update Muon trigger to include OR of TkMu50 (#2)

--- Common/python/trigger_cff.py
+++ Common/python/trigger_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 TriggerMuon = cms.EDFilter("HLTHighLevel",
-				  HLTPaths = cms.vstring("HLT_Mu50_v*"),
+				  HLTPaths = cms.vstring("HLT_Mu50_v*", "HLT_TkMu50_v*"),
 				  throw = cms.bool(False),
 				  TriggerResultsTag = cms.InputTag("TriggerResults", "", "HLT"),
 				  andOr = cms.bool(True), eventSetupPathsKey = cms.string("")#false = and-mode (all requested triggers), true = or-mode (at least one) 
